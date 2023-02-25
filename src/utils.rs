e835ccb0429a347a0dd70d9d88055239ff1c4c80
Jp <joe.pentland@gmail.com>
2023-02-25 07:47:10 +0100

    coredump

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,6 @@
 use cfg_if::cfg_if;
 
 cfg_if! {
-    // https://github.com/rustwasm/console_error_panic_hook#readme
     if #[cfg(feature = "console_error_panic_hook")] {
         extern crate console_error_panic_hook;
         pub use self::console_error_panic_hook::set_once as set_panic_hook;
